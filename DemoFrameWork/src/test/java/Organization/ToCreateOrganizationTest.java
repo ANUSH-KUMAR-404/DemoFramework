2b6e40e3086fe273b73a6251876628ad8a413ed0
UMA MAHESH <UMA MAHESH@LAPTOP-3RU2HEOP>
2024-11-05 08:32:18 +0530

    First push

---
+++ DemoFrameWork/src/test/java/Organization/ToCreateOrganizationTest.java
@@ -0,0 +1,11 @@
+package Organization;
+
+public class ToCreateOrganizationTest {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("Organization created successfully");
+	}
+
+}
